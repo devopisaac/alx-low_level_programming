08f343725fe1f06efca4e08d722722f6cead74e3
devopisaac <devop.isaac@gmail.com>
2022-09-12 12:02:39 -0700

    print type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 
 	printf("Size of a char: %u byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %u byte(s)\n", sizeof(ainteger));
-	printf("Size of a  long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
