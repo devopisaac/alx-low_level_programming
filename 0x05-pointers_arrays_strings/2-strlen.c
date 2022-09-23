98f93351b99a3a63a3feabc757a3096665236d0f
devopisaac <devop.isaac@gmail.com>
2022-09-22 20:08:57 -0700

    return string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
+#include<stdio.h>
 #include "main.h"
-#include <unistd.h>
 /**
- *_puts - prints a string
- *@str: value to evaluate.
- *Return: not.
- */
-void _puts(char *str)
+*_strlen - returns string length.
+*@s: value to evaluate.
+*Return: 0
+*/
+int _strlen(char *s)
 {
-	int l = 0;
+	int len = 0;
 
-	while (*(str + l) != '\0')
+	while (*s != '\0')
 	{
-		_putchar(str[l]);
-		l++;
+		s++;
+		len++;
 	}
-	_putchar('\n');
+	return (len);
 }
