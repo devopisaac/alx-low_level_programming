d6b992f719821e47c0f13e81c82f4e68f72b476a
devopisaac <devop.isaac@gmail.com>
2022-09-30 21:08:47 -0700

    print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints arguenments count received
+ * @argc: arguemnet count
+ * @argv: array of pointers to argument strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
