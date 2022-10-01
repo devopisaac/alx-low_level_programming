8f73f2bb77b4be4f6e41b9e9e572144aec3f63f3
devopisaac <devop.isaac@gmail.com>
2022-09-30 21:04:56 -0700

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  writes program name
+ * @argc: arguement count
+ * @argv: array containing pointers to argument strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
